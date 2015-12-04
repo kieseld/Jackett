c3ef50be316ba2fc2d0cacb44e3ccd4baf902eec
Raspdealer <raspdealer@users.noreply.github.com>
2015-12-04 20:39:30 +0100

    Update Build-mono.bat

--- Build-mono.bat
+++ Build-mono.bat
@@ -1,6 +1,4 @@
-
-rmdir /s /q  build.windows
-rmdir /s /q  build.mono
+rmdir /s /q  Jackett.Binaries.Mono
 rmdir /s /q  Output
 
 
@@ -8,14 +6,14 @@ cd src
 call "C:\Program Files (x86)\Mono\bin\xbuild.bat"  Jackett.sln /t:Build /p:Configuration=Release /verbosity:minimal
 cd ..
 
-xcopy src\Jackett.Console\bin\Release build.mono\  /e /y
-copy /Y src\Jackett.Service\bin\Release\JackettService.exe build.mono\JackettService.exe
-copy /Y src\Jackett.Service\bin\Release\JackettService.exe.config build.mono\JackettService.exe.config
-copy /Y src\Jackett.Tray\bin\Release\JackettTray.exe build.mono\JackettTray.exe
-copy /Y src\Jackett.Tray\bin\Release\JackettTray.exe.config build.mono\JackettTray.exe.config
-copy /Y LICENSE build.mono\LICENSE
-copy /Y README.md build.mono\README.md
-copy /Y Upstart.config build.mono\Upstart.config
+xcopy src\Jackett.Console\bin\Release Jackett.Binaries.Mono\Jackett\  /e /y
+copy /Y src\Jackett.Service\bin\Release\JackettService.exe Jackett.Binaries.Mono\Jackett\JackettService.exe
+copy /Y src\Jackett.Service\bin\Release\JackettService.exe.config Jackett.Binaries.Mono\Jackett\JackettService.exe.config
+copy /Y src\Jackett.Tray\bin\Release\JackettTray.exe Jackett.Binaries.Mono\Jackett\JackettTray.exe
+copy /Y src\Jackett.Tray\bin\Release\JackettTray.exe.config Jackett.Binaries.Mono\Jackett\JackettTray.exe.config
+copy /Y LICENSE Jackett.Binaries.Mono\Jackett\LICENSE
+copy /Y README.md Jackett.Binaries.Mono\Jackett\README.md
+copy /Y Upstart.config Jackett.Binaries.Mono\Jackett\Upstart.config
 
-iscc Installer.iss
-pause
+cd Jackett.Binaries.Mono\Jackett\
+del "*.xml"
